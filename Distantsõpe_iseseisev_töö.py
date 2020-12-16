71d93339baa4121f1590c3303f2fa1892713d471
Mattiasnobot <75736541+Mattiasnobot@users.noreply.github.com>
2020-12-16 22:18:57 +0200

    Update Distantsõpe_iseseisev_töö.py

--- Distantsõpe_iseseisev_töö.py
+++ Distantsõpe_iseseisev_töö.py
@@ -3,21 +3,29 @@
 #Iseseisvad ülessanded
 
 #Ülessanne 4
-print("¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤\n"
-      "¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤\n"
-      "¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤\n"
-      "¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤\n"
-      "==============================================\n"
-      "==============================================\n"
-      "==============================================\n"
-      "==============================================\n"
-      "----------------------------------------------\n"
-      "----------------------------------------------\n"
-      "----------------------------------------------\n"
-      "----------------------------------------------\n")
+#Eesti lipp
+for i in range(1,5):
+    print('\n')
+    for j in range(1,70+1):       
+        print('¤', end='')
 
+for i in range(1,4):
+    print('\n')
+    for j in range(1,70+1):      
+        print('=', end='')
+
+for i in range(1,4):
+    print('\n')
+    for j in range(1,70+1):     
+        print('-', end='')
+
+print("\n")
+print("\n")
+print("\n")
+print("\n")
 
 #Ülessanne 12
+
 #kolmnurk
 print("*\n"
       "***\n"
@@ -47,21 +55,25 @@ for i in range(1,maks):
 
 
 #Ülessanne 8
+#tagurpidi lause
 laused = input("Sisesta lause:")
 laused = laused.split()
 laused = list(reversed(laused))
 print(" ".join(laused))
 
 print("---------\n")
+
 #Ülessanne 9
+#nr 1-66
 for i in range(1,66+1):
     print(i,end=" ")
 
-print("------------------\n")
+print("\n")
 
 
 
 #Ülessanne 1
+#liitmine
 arv1 = int(input("Sisesta esimene arv mida soovid liita: "))
 arv2 = int(input("Sisesta teine arv mida soovid liita: "))
 liitmine = arv1 + arv2
